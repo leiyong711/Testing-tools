e15115475f8e68c7e3976d32d88f621a45c94a55
leiyong711 <leiyong711@163.com>
2017-07-14 10:16:15 +0800

    完成警告弹窗

--- gui_tk.py
+++ gui_tk.py
@@ -9,38 +9,49 @@ import os
 import time
 import Tkinter
 import ttk
-
-
+import tkMessageBox
+from Tkinter import *
 
 root = Tkinter.Tk()
 root.iconbitmap('hah.ico')
 root.title('测试小工具(Last indulge)')
-root.geometry('930x690')
+root.geometry('930x730')
 
 canvas = Tkinter.Canvas(root, width=1920, height=1080, bg='#EAEAEA')
 canvas.pack()
+
+# 在大窗口下定义一个顶级菜单实例
+menubar = Menu(root)
+
+def xp():
+    tkMessageBox.askyesno('帮助', '如发现BUG请通过邮件\nleiyong711@163.com\n联系作者')
+menubar.add_command(label='帮助',command=xp)
+
 
 
 def devices():
     xx = []
     def modified(name):
-        try:
-            reu = list(os.popen(name).readlines())
-            return re.findall('.*', reu[0])[0]  # ([^\s\\\]+)
-        except:
-            return u'该机型获取信息异常'
+        # try:
+        reu = list(os.popen(name).readlines())
+        return re.findall('.*', reu[0])[0]  # ([^\s\\\]+)
+        # except:
+        #     return u'该机型获取信息异常'
+    try:
+        brand = modified('adb shell getprop ro.product.brand')  # 读取手机品牌
 
-    brand = modified('adb shell getprop ro.product.brand')  # 读取手机品牌
-    phone_models = modified('adb shell getprop ro.semc.product.name')  # 读取设备型号
-    deviceVersion = modified('adb shell getprop ro.build.version.release')  # 读取设备系统版本号
-    readDeviceId = list(os.popen('adb devices').readlines())  # 读取设备 id
-    device = str(readDeviceId[1])[:-8]
-    if device == '':
-        device = '该机型获取信息异常'
-    pp_win.insert(0, brand)
-    xh_win.insert(0, phone_models)
-    bb_win.insert(0, deviceVersion)
-    devices_win.insert(0, device)
+        phone_models = modified('adb shell getprop ro.semc.product.name')  # 读取设备型号
+        deviceVersion = modified('adb shell getprop ro.build.version.release')  # 读取设备系统版本号
+        readDeviceId = list(os.popen('adb devices').readlines())  # 读取设备 id
+        device = str(readDeviceId[1])[:-8]
+        if device == '':
+            device = '该机型获取信息异常'
+        pp_win.insert(0, brand)
+        xh_win.insert(0, phone_models)
+        bb_win.insert(0, deviceVersion)
+        devices_win.insert(0, device)
+    except:
+        tkMessageBox.showerror('警告', '请查看设备是否正常连上PC\nUSB调试是否打开')
 
 
 def ApkInfo(path, name, search):
@@ -66,10 +77,15 @@ def ApkInfo(path, name, search):
 def aaptx():
     path = path_win.get()
     name = name_win.get()
-    appPackage = ApkInfo(path, name, 'package: name=')  # 获取应用包名
-    appb_win.insert(0, appPackage)
-    appActivity = ApkInfo(path, name, 'launchable-activity: name=')  # 获取启动类
-    appa_win.insert(0, appActivity)
+    if path == '':
+        tkMessageBox.showerror('警告', '请填写正确的应用路径')
+    elif name == '':
+        tkMessageBox.showerror('警告', '请填写正确的应用名')
+    else:
+        appPackage = ApkInfo(path, name, 'package: name=')  # 获取应用包名
+        appb_win.insert(0, appPackage)
+        appActivity = ApkInfo(path, name, 'launchable-activity: name=')  # 获取启动类
+        appa_win.insert(0, appActivity)
 
 
 
@@ -95,9 +111,14 @@ def qdsj():
     ti = int(ceshu_ac.get())
     packagename = qbao_win.get()
     activity = qActivity_win.get()
-    os.popen('adb shell am force-stop %s' % packagename)
-    for i in range(ti):
-        qds_win.insert(0, str(cs(packagename, activity)) + 'ms')
+    if packagename == '':
+        tkMessageBox.showerror('警告', '请填写测试包名')
+    elif activity == '':
+        tkMessageBox.showerror('警告', '请填写测试包Activity')
+    else:
+        os.popen('adb shell am force-stop %s' % packagename)
+        for i in range(ti):
+            qds_win.insert(0, str(cs(packagename, activity)) + 'ms')
 
 
 
@@ -149,6 +170,7 @@ ram_win = Tkinter.Listbox(root, width=30, height=4)  # 内存输出
 qds_win = Tkinter.Listbox(root, width=30, height=6)  # 内存输出
 
 # 文字控件配置
+cx = Tkinter.Label(root, text='信息查询', bg='#EAEAEA', fg='red', font=("黑体", 21, "bold"))
 pplabel = Tkinter.Label(root, text='手机品牌：', bg='#EAEAEA')
 xhlabel = Tkinter.Label(root, text='型号：', bg='#EAEAEA')
 bblabel = Tkinter.Label(root, text='Android版本：', bg='#EAEAEA')
@@ -157,12 +179,7 @@ pathlabel = Tkinter.Label(root, text='应用路径：', bg='#EAEAEA')
 namelabel = Tkinter.Label(root, text='应用名：', bg='#EAEAEA')
 appblabel = Tkinter.Label(root, text='应用包名：', bg='#EAEAEA')
 appalabel = Tkinter.Label(root, text='启动类名：', bg='#EAEAEA')
-xn = Tkinter.Label(root, text='性能参数展示', bg='#EAEAEA', fg='red', font=("黑体", 21, "bold"))
-cpulabel = Tkinter.Label(root, text='cpu', bg='#EAEAEA')
-liullabel = Tkinter.Label(root, text='流量', bg='#EAEAEA')
-cs_label = Tkinter.Label(root, text='参数次数：', bg='#EAEAEA')
-ramlabel = Tkinter.Label(root, text='内存', bg='#EAEAEA')
-baolabel = Tkinter.Label(root, text='包名：', bg='#EAEAEA')
+
 qd = Tkinter.Label(root, text='启动时间测试', bg='#EAEAEA', fg='red', font=("黑体", 21, "bold"))
 qbaolabel = Tkinter.Label(root, text='测试包名：', bg='#EAEAEA')
 qActivitylabel = Tkinter.Label(root, text='测试包Activity：', bg='#EAEAEA')
@@ -174,62 +191,57 @@ monkeyslabel = Tkinter.Label(root, text='随机种子数量：', bg='#EAEAEA')
 monkeytilabel = Tkinter.Label(root, text='时间间隔：', bg='#EAEAEA')
 
 # 下拉框布局
-canvas.create_window(895, 225, window=cishu_ac)  # 性能测试‘参数次数’按钮布局
-canvas.create_window(895, 410, window=ceshu_ac)  # 性能测试‘测试次数’按钮布局
+
+canvas.create_window(895, 265, window=ceshu_ac)  # 性能测试‘测试次数’按钮布局
 canvas.create_window(895, 620, window=jg_ac)  # Mokey测试‘时间间隔’按钮布局
 
 # 按钮布局
-canvas.create_window(780, 30, window=devicesb)  # 性能测试‘查询设备信息’按钮布局
-canvas.create_window(780, 112, window=app)  # 性能测试‘查询设备信息’按钮布局
-canvas.create_window(400, 320, window=ks)  # 性能测试‘开始测试’按钮布局
-canvas.create_window(400, 490, window=csb)  # 启动时间‘启动测试’按钮布局
+canvas.create_window(780, 85, window=devicesb)  # 性能测试‘查询设备信息’按钮布局
+canvas.create_window(780, 167, window=app)  # 性能测试‘查询设备信息’按钮布局
+
+canvas.create_window(400, 345, window=csb)  # 启动时间‘启动测试’按钮布局
 canvas.create_window(400, 670, window=monkeyb)  # Monkey‘启动测试’按钮布局
 
 # 文字控件布局
-canvas.create_window(80, 15, window=pplabel)  # 品牌文字布局
-canvas.create_window(80, 45, window=xhlabel)  # 型号文字布局
-canvas.create_window(410, 15, window=bblabel)  # Android版本文字布局
-canvas.create_window(410, 45, window=sblabel)  # 设备号文字布局
-canvas.create_window(80, 100, window=pathlabel)  # 应用路径文字布局
-canvas.create_window(80, 125, window=namelabel)  # 应用名文字布局
-canvas.create_window(410, 100, window=appblabel)  # 包名文字布局
-canvas.create_window(410, 125, window=appalabel)  # 启动类号文字布局
-canvas.create_window(400, 165, window=xn)  # 性能测试文字布局
-canvas.create_window(130, 280, window=cpulabel)  # 性能测试‘cpu’文字布局
-canvas.create_window(635, 280, window=liullabel)  # 性能测试‘流量’文字布局
-canvas.create_window(830, 225, window=cs_label)  # 性能测试‘参数次数’文字布局
-canvas.create_window(365, 280, window=ramlabel)  # 性能测试‘内存’文字布局
-canvas.create_window(80, 320, window=baolabel)  # 性能测试‘包名：’文字布局
-canvas.create_window(400, 370, window=qd)  # ‘启动时间测试’文字布局
-canvas.create_window(80, 410, window=qbaolabel)  # 启动时间测试‘测试包名’文字布局
-canvas.create_window(450, 410, window=qActivitylabel)  # 启动时间测试‘测试包Activity’文字布局
-canvas.create_window(830, 410, window=qnumlabel)  # 启动时间测试‘测试次数’文字布局
-canvas.create_window(70, 500, window=qdslabel)  # 启动时间测试‘启动时间展示’文字布局
-canvas.create_window(400, 580, window=monkeylabel)  # Monkey测试文字布局
+canvas.create_window(400, 25, window=cx)  # ‘信息查询’文字布局
+canvas.create_window(80, 70, window=pplabel)  # 品牌文字布局
+canvas.create_window(80, 100, window=xhlabel)  # 型号文字布局
+canvas.create_window(410, 70, window=bblabel)  # Android版本文字布局
+canvas.create_window(410, 100, window=sblabel)  # 设备号文字布局
+canvas.create_window(80, 155, window=pathlabel)  # 应用路径文字布局
+canvas.create_window(80, 185, window=namelabel)  # 应用名文字布局
+canvas.create_window(410, 155, window=appblabel)  # 包名文字布局
+canvas.create_window(410, 185, window=appalabel)  # 启动类号文字布局
+canvas.create_window(400, 230, window=qd)  # ‘启动时间测试’文字布局
+canvas.create_window(80, 265, window=qbaolabel)  # 启动时间测试‘测试包名’文字布局
+canvas.create_window(450, 265, window=qActivitylabel)  # 启动时间测试‘测试包Activity’文字布局
+canvas.create_window(830, 265, window=qnumlabel)  # 启动时间测试‘测试次数’文字布局
+canvas.create_window(70, 345, window=qdslabel)  # 启动时间测试‘启动时间展示’文字布局
+canvas.create_window(400, 430, window=monkeylabel)  # Monkey测试文字布局
 canvas.create_window(70, 620, window=monkeybaolabel)  # Monkey测试‘测试包名’文字布局
 canvas.create_window(450, 620, window=monkeyslabel)  # Monkey测试‘随机种子数量’文字布局
 canvas.create_window(830, 620, window=monkeytilabel)  # Monkey测试‘种子间隔’文字布局
 
 # 输入文本布局
-canvas.create_window(230, 100, window=path_win)  # ‘应用路径：’输入文本布局
-canvas.create_window(230, 125, window=name_win)  # ‘应用名：’输入文本布局
-canvas.create_window(230, 320, window=baoi_win)  # 性能测试‘包名：’输入文本布局
-canvas.create_window(230, 410, window=qbao_win)  # 启动时间测试‘测试包名：’输入文本布局
-canvas.create_window(650, 410, window=qActivity_win)  # 启动时间测试‘测试包Activity：’输入文本布局
+canvas.create_window(230, 155, window=path_win)  # ‘应用路径：’输入文本布局
+canvas.create_window(230, 185, window=name_win)  # ‘应用名：’输入文本布局
+canvas.create_window(230, 265, window=qbao_win)  # 启动时间测试‘测试包名：’输入文本布局
+canvas.create_window(650, 265, window=qActivity_win)  # 启动时间测试‘测试包Activity：’输入文本布局
 canvas.create_window(230, 620, window=monkeybao_win)  # Mokey测试‘测试包名：’输入文本布局
 canvas.create_window(650, 620, window=monkeysl_win)  # Mokey测试‘随机种子数量：’输入文本布局
 
 
 # 输出文本布局
-canvas.create_window(230, 15, window=pp_win)  # 性能测试‘品牌’输出文本布局
-canvas.create_window(230, 45, window=xh_win)  # 性能测试‘型号’输出文本布局
-canvas.create_window(580, 15, window=bb_win)  # 性能测试‘’输出文本布局
-canvas.create_window(580, 45, window=devices_win)  # 性能测试‘设备号’输出文本布局
-canvas.create_window(580, 100, window=appb_win)  # ‘应用包名’输出文本布局
-canvas.create_window(580, 125, window=appa_win)  # ‘启动类’输出文本布局
-canvas.create_window(130, 225, window=cpu_win)  # 性能测试‘cpu’输出文本布局
-canvas.create_window(635, 225, window=liul_win)  # 性能测试‘流量’输出文本布局
-canvas.create_window(365, 225, window=ram_win)  # 性能测试‘内存’输出文本布局
-canvas.create_window(230, 500, window=qds_win)  # 启动测试‘启动时间展示’输出文本布局
+canvas.create_window(230, 70, window=pp_win)  # 性能测试‘品牌’输出文本布局
+canvas.create_window(230, 100, window=xh_win)  # 性能测试‘型号’输出文本布局
+canvas.create_window(580, 70, window=bb_win)  # 性能测试‘’输出文本布局
+canvas.create_window(580, 100, window=devices_win)  # 性能测试‘设备号’输出文本布局
+canvas.create_window(580, 155, window=appb_win)  # ‘应用包名’输出文本布局
+canvas.create_window(580, 185, window=appa_win)  # ‘启动类’输出文本布局
+
+canvas.create_window(230, 344, window=qds_win)  # 启动测试‘启动时间展示’输出文本布局
+
+# 菜单实例应用到大窗口中
+root['menu']=menubar
 
 root.mainloop()
